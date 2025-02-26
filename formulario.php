102aa3fb8767aade4c1b8beecca699072af840e9
Marcy_ <marciagisseli21@gmail.com>
2025-02-26 11:53:32 -0300

    alinhando o formulário no centro

--- formulario.php
+++ formulario.php
@@ -1,9 +1,15 @@
-
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+<style>
+    .formulario{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 50px;
+    }   
+</style>
 <section class="linha-formulario">
     <div class="formulario">
+        <h1>Formulario</h1>
         <!-- metodo de envio -> GET: manda informações atraves da url E POST: manda informações atraves do corpo -->
         <!-- Action: ele é para onde deve enviar os dados -->
         <form action="./aluno-cadastrar.php" method="POST">
